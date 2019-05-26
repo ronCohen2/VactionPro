a14c954d1f5e5f198e07aab69bdbac932685272d
ronCohen2 <47660576+ronCohen2@users.noreply.github.com>
2019-05-26 13:03:50 +0300

    create api router

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+const apiRouter = require("./routes/api");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
